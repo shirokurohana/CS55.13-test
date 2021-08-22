14ff499f4a2ad4ac9aba60d4561e821ca2d87b63
Nathalie Baladejo-Reynosa (nati) <67033740+shirokurohana@users.noreply.github.com>
2021-08-22 03:30:33 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log("Sup!");
+// this is a single-line javascript comment
+
+let myhttp = require("http");
+
+// myhttp.createrServer( );
+// take some arguments to make it provide a useful http server
+// 1: function that will be run whenever the server receives an http request
+
+
+let myserver = myhttp.createServer(
+  // createServer() uses our function
+  function (myrequest, myresponse) {
+    console.log(myrequest.url);
+    let mytext;
+    if (myrequest.url === "/hey") {
+      mytext === "well hello there...";
+
+    } else {
+      mytext === "I don't know you";    }
+
+    myresponse.writeHead( 200, {"Content-Type": "text/plain"});
+    // writeHead() creates an http response header
+
+    // end() closes the response
+    myresponse.end(mytext + "\n");
+  }
+);
+
+// ask http 
+// listen() takes 2 args
+myserver.listen(8080, "0.0.0.0");
+console.log("server has started");
